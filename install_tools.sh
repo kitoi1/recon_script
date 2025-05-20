d1e0a86ae12f7cdcf2072c27b78a7a02efd0673d
kitoi1 <kasaufx@gmail.com>
2025-05-21 01:24:00 +0300

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,41 @@
+
+## install_tools.sh
+
+```bash
+#!/bin/bash
+
+# Recon Suite Tool Installer
+
+echo "[*] Installing required tools..."
+
+# Install from package manager
+sudo apt-get update
+sudo apt-get install -y jq parallel golang python3-pip
+
+# Install Go tools
+GO_TOOLS=(
+    "github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
+    "github.com/projectdiscovery/assetfinder@latest"
+    "github.com/OWASP/Amass/v3/...@master"
+    "github.com/Edu4rdSHL/findomain@latest"
+    "github.com/projectdiscovery/dnsx/cmd/dnsx@latest"
+    "github.com/projectdiscovery/httpx/cmd/httpx@latest"
+    "github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest"
+    "github.com/lc/gau/v2/cmd/gau@latest"
+    "github.com/tomnomnom/waybackurls@latest"
+    "github.com/haccer/subjack@latest"
+    "github.com/projectdiscovery/naabu/v2/cmd/naabu@latest"
+    "github.com/ffuf/ffuf@latest"
+    "github.com/michenriksen/aquatone@latest"
+)
+
+for tool in "${GO_TOOLS[@]}"; do
+    echo "[*] Installing $tool"
+    go install $tool
+done
+
+# Add Go binaries to PATH
+echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
+source ~/.bashrc
+
+echo "[+] Installation complete!"
